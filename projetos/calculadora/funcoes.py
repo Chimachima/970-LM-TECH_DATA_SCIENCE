21c3243427a98d9d17f5f99c24635093329a9909
Abelar <abelar.henrique@gmail.com>
2023-03-29 20:11:34 -0300

    Correcoes

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -1,11 +1,20 @@
 def soma(a,b):
-    return a+b
+    if isinstance(a,(int,float)) and isinstance(b,(int,float)):
+        return a+b
+    else:
+        raise TypeError (f"O input deve ser int ou float. Recebido: a = {a}, tipo = {type(a)}, b = {b}, tipo = {type(b)}")
+    
 
 def subtracao(a,b):
     return a-b
 
 def multiplicacao(a,b):
-    return a*b
+    if isinstance(a,(int,float)) and isinstance(b,(int,float)):
+        return a*b
+    else:
+        raise TypeError (f"O input deve ser int ou float. Recebido: a = {a}, tipo = {type(a)}, b = {b}, tipo = {type(b)}")
 
 def divisao(a,b):
+    if b == 0:
+        return 0
     return a/b
